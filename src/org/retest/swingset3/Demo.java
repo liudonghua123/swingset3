7df4995c826c685f205a99b5dfb0b04a31d430ec
Daniel Kraus <daniel.kraus@mailbox.org>
2017-04-28 15:20:15 +0200

    Revert cc0593e
    Causes problems after build.

--- src/org/retest/swingset3/Demo.java
+++ src/org/retest/swingset3/Demo.java
@@ -34,18 +34,17 @@ package org.retest.swingset3;
 import java.awt.Component;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
-import java.io.File;
 import java.lang.reflect.Method;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.*;
+import java.util.ArrayList;
 import java.util.logging.Level;
+
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
 
 /**
  * Wrapper class which encapsulates a GUI component to be displayed as a SwingSet3 demo.
- * 
+ *
  * @author Amy Fowler
  */
 public class Demo {
@@ -61,17 +60,17 @@ public class Demo {
 
 	private static final String IMAGE_EXTENSIONS[] = { ".gif", ".png", ".jpg" };
 
-	public static String deriveCategoryFromPackageName( String className ) {
-		String parts[] = className.split( "\\." );
+	public static String deriveCategoryFromPackageName( final String className ) {
+		final String parts[] = className.split( "\\." );
 		// return the right-most package name
 		return parts.length >= 2 ? parts[parts.length - 2] : "general";
 	}
 
-	public static String deriveCategoryFromPackageName( Class demoClass ) {
+	public static String deriveCategoryFromPackageName( final Class demoClass ) {
 		String packageName = demoClass.getPackage() != null ? demoClass.getPackage().getName() : null;
 		if ( packageName != null ) {
 			// if root package is swingset3, then remove it
-			String swingsetPackageName = Demo.class.getPackage().getName();
+			final String swingsetPackageName = Demo.class.getPackage().getName();
 			if ( packageName.startsWith( swingsetPackageName + ".demos." ) ) {
 				packageName = packageName.replaceFirst( swingsetPackageName + ".demos.", "" );
 			}
@@ -79,18 +78,18 @@ public class Demo {
 		return packageName != null ? packageName : "general";
 	}
 
-	public static String deriveNameFromClassName( String className ) {
-		String simpleName = className.substring( className.lastIndexOf( "." ) + 1, className.length() );
+	public static String deriveNameFromClassName( final String className ) {
+		final String simpleName = className.substring( className.lastIndexOf( "." ) + 1, className.length() );
 		return convertToDemoName( simpleName );
 	}
 
-	public static String deriveNameFromClassName( Class demoClass ) {
-		String className = demoClass.getSimpleName();
+	public static String deriveNameFromClassName( final Class demoClass ) {
+		final String className = demoClass.getSimpleName();
 		return convertToDemoName( className );
 	}
 
-	private static String convertToDemoName( String simpleClassName ) {
-		StringBuffer nameBuffer = new StringBuffer();
+	private static String convertToDemoName( final String simpleClassName ) {
+		final StringBuffer nameBuffer = new StringBuffer();
 		if ( simpleClassName.endsWith( "Demo" ) ) {
 			nameBuffer.append( simpleClassName.substring( 0, simpleClassName.indexOf( "Demo" ) ) );
 			nameBuffer.append( " " );
@@ -99,7 +98,7 @@ public class Demo {
 		return nameBuffer.toString();
 	}
 
-	private Class<?> demoClass;
+	private final Class<?> demoClass;
 	private String name;
 	private String category;
 	private String shortDescription; // used for tooltips
@@ -116,7 +115,7 @@ public class Demo {
 
 	private PropertyChangeSupport pcs;
 
-	public Demo( Class<?> demoClass ) {
+	public Demo( final Class<?> demoClass ) {
 		this.demoClass = demoClass;
 
 		initializeProperties();
@@ -125,17 +124,17 @@ public class Demo {
 	protected void initializeProperties() {
 
 		// First look for DemoProperties annotation if it exists
-		DemoProperties properties = demoClass.getAnnotation( DemoProperties.class );
+		final DemoProperties properties = demoClass.getAnnotation( DemoProperties.class );
 		if ( properties != null ) {
-			this.name = properties.value();
-			this.category = properties.category();
-			this.shortDescription = properties.description();
-			this.iconPath = properties.iconFile();
-			this.sourceFilePaths = properties.sourceFiles();
+			name = properties.value();
+			category = properties.category();
+			shortDescription = properties.description();
+			iconPath = properties.iconFile();
+			sourceFilePaths = properties.sourceFiles();
 		} else {
-			this.name = deriveNameFromClassName( demoClass );
-			this.category = deriveCategoryFromPackageName( demoClass );
-			this.shortDescription = "No demo description, run it to find out...";
+			name = deriveNameFromClassName( demoClass );
+			category = deriveCategoryFromPackageName( demoClass );
+			shortDescription = "No demo description, run it to find out...";
 		}
 		state = State.UNINITIALIZED;
 		pcs = new PropertyChangeSupport( this );
@@ -160,7 +159,7 @@ public class Demo {
 				icon = getIconFromPath( iconPath );
 			} else {
 				// Look for icon with same name as demo class
-				for ( String ext : IMAGE_EXTENSIONS ) {
+				for ( final String ext : IMAGE_EXTENSIONS ) {
 					icon = getIconFromPath( getIconImagePath( ext ) );
 					if ( icon != null ) {
 						break;
@@ -171,14 +170,14 @@ public class Demo {
 		return icon;
 	}
 
-	protected String getIconImagePath( String extension ) {
+	protected String getIconImagePath( final String extension ) {
 		// by default look for an image with the same name as the demo class
 		return "resources/images/" + demoClass.getSimpleName() + extension;
 	}
 
-	private Icon getIconFromPath( String path ) {
+	private Icon getIconFromPath( final String path ) {
 		Icon icon = null;
-		URL imageURL = demoClass.getResource( path );
+		final URL imageURL = demoClass.getResource( path );
 		if ( imageURL != null ) {
 			icon = new ImageIcon( imageURL );
 		}
@@ -197,24 +196,23 @@ public class Demo {
 	public URL[] getSourceFiles() {
 		// If not already cached, then look them up
 		if ( sourceFiles == null ) {
-			ArrayList<URL> pathURLs = new ArrayList<URL>();
+			final ArrayList<URL> pathURLs = new ArrayList<>();
 
 			// Get any names registered in DemoProperties meta-data.
 			// If meta-data is not specified then sourceFilePaths contains
 			// one empty string. In this case we skip it.
 			if ( !(sourceFilePaths.length == 1 && sourceFilePaths[0].length() == 0) ) {
-				for ( String path : sourceFilePaths ) {
-					try {
-						File sourceFile = new File( "src/" + path );
-						URL url = sourceFile.toURI().toURL();
-						pathURLs.add( url );
-					} catch ( MalformedURLException e ) {
+				for ( final String path : sourceFilePaths ) {
+					final URL url = getClass().getClassLoader().getResource( path );
+					if ( url == null ) {
 						SwingSet3.logger.log( Level.WARNING, "unable to load source file '" + path + "'" );
+					} else {
+						pathURLs.add( url );
 					}
 				}
 			}
 
-			this.sourceFiles = pathURLs.toArray( new URL[pathURLs.size()] );
+			sourceFiles = pathURLs.toArray( new URL[pathURLs.size()] );
 		}
 
 		return sourceFiles;
@@ -224,15 +222,15 @@ public class Demo {
 		setState( Demo.State.INITIALIZING );
 	}
 
-	void setDemoComponent( Component component ) {
+	void setDemoComponent( final Component component ) {
 		if ( component != null && !demoClass.isInstance( component ) ) {
 			setState( State.FAILED );
-			IllegalArgumentException e =
+			final IllegalArgumentException e =
 					new IllegalArgumentException( "component must be an instance of " + demoClass.getCanonicalName() );
 			failException = e;
 			throw e;
 		}
-		Component old = this.component;
+		final Component old = this.component;
 		this.component = component;
 
 		if ( component != null ) {
@@ -249,7 +247,7 @@ public class Demo {
 		try {
 			component = (Component) demoClass.newInstance();
 			setDemoComponent( component );
-		} catch ( Exception e ) {
+		} catch ( final Exception e ) {
 			System.err.println( e );
 			e.printStackTrace();
 			failException = e;
@@ -266,37 +264,37 @@ public class Demo {
 		return state;
 	}
 
-	protected void setState( State state ) {
-		State oldState = this.state;
+	protected void setState( final State state ) {
+		final State oldState = this.state;
 		this.state = state;
 		SwingSet3.logger.log( Level.FINE, "***>" + getName() + ":setState=" + state );
 		pcs.firePropertyChange( "state", oldState, state );
 	}
 
-	public void addPropertyChangeListener( PropertyChangeListener pcl ) {
+	public void addPropertyChangeListener( final PropertyChangeListener pcl ) {
 		pcs.addPropertyChangeListener( pcl );
 	}
 
-	public void removePropertyChangeListener( PropertyChangeListener pcl ) {
+	public void removePropertyChangeListener( final PropertyChangeListener pcl ) {
 		pcs.removePropertyChangeListener( pcl );
 	}
 
 	private void init() {
 		setState( State.INITIALIZED );
 		try {
-			Method initMethod = demoClass.getMethod( "init", (Class[]) null );
+			final Method initMethod = demoClass.getMethod( "init", (Class[]) null );
 			initMethod.invoke( component, (Object[]) null );
-		} catch ( NoSuchMethodException nsme ) {
+		} catch ( final NoSuchMethodException nsme ) {
 			// okay, no init method exists
-		} catch ( IllegalAccessException iae ) {
+		} catch ( final IllegalAccessException iae ) {
 			SwingSet3.logger.log( Level.SEVERE, "unable to init demo: " + demoClass.getName(), iae );
 			failException = iae;
 			setState( State.FAILED );
-		} catch ( java.lang.reflect.InvocationTargetException ite ) {
+		} catch ( final java.lang.reflect.InvocationTargetException ite ) {
 			SwingSet3.logger.log( Level.SEVERE, "init method failed for demo: " + demoClass.getName(), ite );
 			failException = ite;
 			setState( State.FAILED );
-		} catch ( NullPointerException npe ) {
+		} catch ( final NullPointerException npe ) {
 			SwingSet3.logger.log( Level.SEVERE,
 					"init method called before demo was instantiated: " + demoClass.getName(), npe );
 			failException = npe;
@@ -307,21 +305,21 @@ public class Demo {
 	public void start() {
 
 		try {
-			Method startMethod = demoClass.getMethod( "start", (Class[]) null );
+			final Method startMethod = demoClass.getMethod( "start", (Class[]) null );
 			startMethod.invoke( component, (Object[]) null );
 			setState( State.RUNNING );
-		} catch ( NoSuchMethodException nsme ) {
+		} catch ( final NoSuchMethodException nsme ) {
 			setState( State.RUNNING );
 			// okay, no start method exists
-		} catch ( IllegalAccessException iae ) {
+		} catch ( final IllegalAccessException iae ) {
 			SwingSet3.logger.log( Level.SEVERE, "unable to start demo: " + demoClass.getName(), iae );
 			failException = iae;
 			setState( State.FAILED );
-		} catch ( java.lang.reflect.InvocationTargetException ite ) {
+		} catch ( final java.lang.reflect.InvocationTargetException ite ) {
 			SwingSet3.logger.log( Level.SEVERE, "start method failed for demo: " + demoClass.getName(), ite );
 			failException = ite;
 			setState( State.FAILED );
-		} catch ( NullPointerException npe ) {
+		} catch ( final NullPointerException npe ) {
 			SwingSet3.logger.log( Level.SEVERE,
 					"start method called before demo was instantiated: " + demoClass.getName(), npe );
 			failException = npe;
@@ -332,21 +330,21 @@ public class Demo {
 	public void stop() {
 		setState( State.STOPPED );
 		try {
-			Method stopMethod = demoClass.getMethod( "stop", (Class[]) null );
+			final Method stopMethod = demoClass.getMethod( "stop", (Class[]) null );
 			stopMethod.invoke( component, (Object[]) null );
 
-		} catch ( NoSuchMethodException nsme ) {
+		} catch ( final NoSuchMethodException nsme ) {
 			// okay, no stop method exists
 
-		} catch ( IllegalAccessException iae ) {
+		} catch ( final IllegalAccessException iae ) {
 			SwingSet3.logger.log( Level.SEVERE, "unable to stop demo: " + demoClass.getName(), iae );
 			failException = iae;
 			setState( State.FAILED );
-		} catch ( java.lang.reflect.InvocationTargetException ite ) {
+		} catch ( final java.lang.reflect.InvocationTargetException ite ) {
 			SwingSet3.logger.log( Level.SEVERE, "stop method failed for demo: " + demoClass.getName(), ite );
 			failException = ite;
 			setState( State.FAILED );
-		} catch ( NullPointerException npe ) {
+		} catch ( final NullPointerException npe ) {
 			SwingSet3.logger.log( Level.SEVERE,
 					"stop method called before demo was instantiated: " + demoClass.getName(), npe );
 		}
